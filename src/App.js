e48a4f8340e834723abd49a47f67c9e80a51c32c
Mighty Chen <mchen73@ucmerced.edu>
2018-11-02 11:27:42 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+var body = {
+    username: 'mchen73@ucmerced.edu',
+    password: 'lol',
+}
+
+var server = 'http://10.34.99.54:4000/auth'
+
+class Button extends Component {
+  submit(user, password) {
+    axios.post(
+      server, {user, password})
+      .then(res =>  {
+        console.log(res);
+        console.log(res.data);
+      })
+
+  }
+
+  render() {
+    return (
+      <div>
+        <div>{this.props.username}</div>
+        <div>{this.props.password}</div>
+        <button onClick = {(e) => {this.submit(this.props.username, this.props.password)}}>
+          Login
+        </button>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
+  constructor() {
+    super()
+    this.data = { data: body}
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Button username={this.data.data.username} password={this.data.data.password}/>
         </header>
       </div>
+
     );
   }
 }
