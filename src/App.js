71ddc9881e390a2b113b61cf5c000614000cd070
Mighty Chen <mchen73@ucmerced.edu>
2018-11-12 12:54:35 -0800

    Login Page

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import axios from 'axios';
 import Login from "./components/Login.js";
 
 class App extends Component {
   constructor() {
     super()
+    this.authenticate = this.authenticate.bind(this);
+    this.state = {
+      loggedIn: false,
+      user: {
+        id: -1,
+        first_name: "User",
+        last_name: "User",
+        user_name: "something@ucm.edu",
+        user_type: -1
+      }
+    }
+  }
+
+  authenticate(user) {
+    console.log('hello from authenticate', user)
+    this.setState(
+      { loggedIn: true,
+        user: {
+          id: user[0][0],
+          first_name: user[0][1],
+          last_name: user[0][2],
+          user_name: user[0][3],
+          user_type: user[0][5]
+        }
+      });
+    console.log("State is set.")
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <Login/>
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
+        {console.log(this.state.loggedIn)}
+        { !this.state.loggedIn ?
+          <Login authenticate={this.authenticate}/>
+        :
+        <h1> Logged in as: {this.state.user.first_name} {this.state.user.last_name} </h1>
+
+        }
       </div>
 
     );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,9 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-
-
-var body = {
-    username: 'mchen73@ucmerced.edu',
-    password: 'lol',
-}
+import './Login.css';
 
 var server = 'http://0.0.0.0:4000/auth'
-
-class Textbox extends Component {
-  render() {
-    return(
-      <div>
-        Textbox id is: {this.props.id}
-        <input ref={this.props.id} type="text"/>
-      </div>
-    );
-  }
-}
 
 class Login extends Component {
   constructor(props) {
@@ -27,19 +11,17 @@ class Login extends Component {
     this.submit = this.submit.bind(this);
   }
 
-  authenticated(val) {
-    this.setState( {auth: val});
-  }
-
   submit() {
-    var user = ReactDOM.findDOMNode(this.refs.form_username).value
-    var password = ReactDOM.findDOMNode(this.refs.form_password).value
+    var user = ReactDOM.findDOMNode(this.refs.inputEmail).value
+    var password = ReactDOM.findDOMNode(this.refs.inputPassword).value
 
     axios.post(
       server, {user, password})
       .then(res => {
-        console.log(res);
-        console.log(res.data);
+        // console.log(res);
+        // console.log(this.props.authenticate)
+        console.log(res.data)
+        this.props.authenticate(res.data)
       });
 
     return(
@@ -53,15 +35,38 @@ class Login extends Component {
 
   render() {
     return(
-      <div>
-        <div><input ref="form_username" type="text"/></div>
-        <div><input ref="form_password" type="text"/></div>
-        <button onClick = {(e) => {this.submit()}}>
-          Login
-        </button>
+    // <body id="LoginForm">
+      <div className="container">
+        {/* <h1 className="form-heading">Login</h1> */}
+        <div className="login-form">
+          <div className="main-div">
+            <div className="panel">
+              <h2>Login</h2>
+              <p>Please enter your email and password</p>
+            </div>
+            <form id="Login">
+              <div className="form-group">
+                <input type="email" className="form-control" ref="inputEmail" placeholder="  Email Address"/>
+              </div>
+              <div className="form-group">
+                <input type="password" className="form-control" ref="inputPassword" placeholder="  Password"/>
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={() => {this.submit()}}>
+                Login
+              </button>
+            </form>
+          </div>
+        </div>
       </div>
+    // </body>
     )
   }
 }
 
 export default Login;
+
+
+//https://bootsnipp.com/snippets/featured/login-form
