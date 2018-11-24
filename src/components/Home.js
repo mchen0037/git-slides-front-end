931e87b85fb1da7ebfc13378318678350b24ae3e
Mighty Chen <mchen73@ucmerced.edu>
2018-11-24 10:36:04 -0800

    Used Grids instead of Segments for Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,23 +5,30 @@ import SideNav from './SideNav.js';
 import NavBar from './NavBar.js';
 import BodyContent from './BodyContent.js';
 import { Container, Header, Segment } from 'semantic-ui-react'
+import {Grid} from 'semantic-ui-react';
 
 
 class Home extends Component {
   render() {
     // console.log("Home props:", this.props)
+    // TODO: Different look if it's an exercise or if it's a Slide?
     return(
       <div>
         {this.props.isAuthed ?
           <div>
             <NavBar/>
-            <Container fluid>
-              <Header>
-                <Segment.Group horizontal>
-                  <Segment><SideNav/></Segment>
-                  <Segment attached='top'><BodyContent/></Segment>
-                </Segment.Group>
-              </Header>
+            <Container>
+              <Grid>
+                <Grid.Row width={10}>
+                  {/* FIXME: This might break with a lot of modules. */}
+                  <Grid.Column width={4} style={{overflow: 'auto', maxHeight: 600 }}>
+                    <SideNav/>
+                  </Grid.Column>
+                  <Grid.Column width={12} style={{overflow: 'auto', maxHeight: 600 }}>
+                    <BodyContent/>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
             </Container>
           </div>:
           <div>
